06babfa93711f409bd52b1c9dca8a08448588ef9
root <you@example.com>
2018-12-03 18:45:09 +0800

    1

--- ffmpeg-send-rtmp.sh
+++ ffmpeg-send-rtmp.sh
@@ -1,1 +1,1 @@
-/root/source/ffmpeg-3.4.5/__install/bin/ffmpeg -re -i input.mp4 -c:v libx264 -preset fast -c:a copy -f flv rtmp://localhost/live/test1
+/root/source/ffmpeg-3.4.5/__install/bin/ffmpeg  -re -i input.mp4 -c:v libx264 -preset fast -c:a copy -f flv rtmp://localhost/live/test1
